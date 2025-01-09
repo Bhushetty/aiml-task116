e379c71040b4bcec19ee81851b33114ec8b5163f
geethika <geethikasai576@gmail.com>
2025-01-09 21:18:04 +0530

    Add files via upload

---
+++ geethu.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+def mean_value(*n):
+   sum = 0
+   counter = 0
+   for x in n: 
+       counter = counter +1
+       sum += x
+   mean = sum/counter
+   return mean
+
+
+# In[4]:
+
+
+def median_value(*n):
+    num_list = list(n)
+    num_list.sort() 
+    l = len(num_list)
+    if l%2 == 0:
+       median = (num_list[int(l/2)] + num_list[int((l/2))-1])/2
+    else:
+         median = num_list[int(l/2)]
+    return median
+
+
+# In[9]:
+
+
+mean_value(1,2,3,4,5,6,7,8,9,10)
+
+
+# In[5]:
+
+
+median_value(1,2,3,4,5,6,7,8,9,10)
+
+
+# In[ ]:
+
+
+
+
